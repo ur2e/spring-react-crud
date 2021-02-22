80727c21f4948fd6ef5e5ae26ad5b93eecdde1c3
ur2e <dbfl9711@gmail.com>
2021-02-22 23:50:03 +0900

    add Delete

---
+++ src/main/java/dsctimmyroom/springbootdemo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package dsctimmyroom.springbootdemo.repository;
+
+import dsctimmyroom.springbootdemo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
